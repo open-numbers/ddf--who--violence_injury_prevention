291936ba615724c7bb116e25b40a68e55e2dc065
semio <prairy.long@gmail.com>
2018-10-09 11:27:41 +0800

    check data completeness
    only keep traffic deaths data when 75% or more cases are reported

--- etl/scripts/etl.py
+++ etl/scripts/etl.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import os.path as osp
+import json
 from ddf_utils.str import to_concept_id
 
 
@@ -31,7 +32,7 @@ def preprocess(df, subset):
     return df2
 
 
-def extract(df, concepts_mapping, cause_mapping):
+def extract(df, concepts_mapping, cause_mapping, check_completeness=[], threshold=0.9):
     df.columns = df.columns.map(concepts_mapping)
     df.cause = df.cause.map(cause_mapping)
 
@@ -47,8 +48,11 @@ def extract(df, concepts_mapping, cause_mapping):
     for cause, idx in df.groupby('cause').groups.items():
         df_cause = df.loc[idx].copy()
         df_cause = df_cause.drop('cause', axis=1)
-        min_count = 0.75 * len(cause_mapping_grouped[cause])
-        df_new = df_cause.groupby(['country', 'year', 'sex']).agg("sum", min_count=min_count)
+        if cause in check_completeness:
+            min_count = round(threshold * len(cause_mapping_grouped[cause]))
+        else:
+            min_count =  -1
+        df_new = df_cause.groupby(['country', 'year', 'sex']).sum(min_count=min_count)
         df_new['cause'] = cause
         df_new = df_new.set_index('cause', append=True)
         df_new.reorder_levels(['country', 'year', 'cause', 'sex'])
@@ -159,3 +163,61 @@ def rate_60plus(deaths, pop, pop_div):
     ]
 
     return rate(deaths, pop, pop_div, gs)
+
+
+def read_mort_source():
+    icd7_mapping = json.load(open('./icd7.json'))
+    icd8_mapping = json.load(open('./icd8.json'))
+    icd9_mapping = json.load(open('./icd9.json'))
+    icd10_mapping = json.load(open('./icd10.json'))
+    concept_mapping = json.load(open('./concept_mapping.json'))
+
+    icd7 = pd.read_csv('../source/morticd07.zip')
+    df1 = preprocess(icd7, icd7_mapping.keys())
+    df1 = extract(df1, concept_mapping, icd7_mapping)
+
+    icd8 = pd.read_csv("../source/morticd08.zip")
+    df2 = preprocess(icd8, icd8_mapping.keys())
+    df2 = extract(df2, concept_mapping, icd8_mapping)
+
+    icd9 = pd.read_csv('../source/morticd9.zip')
+    df3 = preprocess(icd9, icd9_mapping.keys())
+    df3 = extract(df3, concept_mapping, icd9_mapping)
+
+    icd101 = pd.read_csv('../source/Morticd10_part1.zip')
+    df4 = preprocess(icd101, icd10_mapping.keys())
+    df4 = extract(df4, concept_mapping, icd10_mapping)
+
+    icd102 = pd.read_csv('../source/Morticd10_part2.zip')
+    df5 = preprocess(icd102, icd10_mapping.keys())
+    df5 = extract(df5, concept_mapping, icd10_mapping)
+
+    df_all = pd.concat([df1, df2, df3, df4, df5])
+    return df_all
+
+
+def read_population_source():
+    pop_div = pd.read_csv("../source/pop.csv")
+    pop_concepts = pd.read_csv('../source/concepts_pop.csv')
+    pop_concepts = pop_concepts.dropna(how='all', axis=1)
+    pop_concepts['concept'] = pop_concepts['Column name'].map(to_concept_id)
+    pop_concepts.loc[3:, 'concept'] = pop_concepts.loc[3:, 'Content'].map(lambda x: to_concept_id(x.replace("at ", "")))
+    pop_concept_mapping = pop_concepts.set_index('Column name')['concept'].to_dict()
+    pop = pd.read_csv('../source/Pop.zip')
+
+    pop["is_country"] = is_country(pop)
+    pop = pop[pop.is_country == True]
+
+    cols = ['Country', 'Year', 'Sex', 'Pop1', 'Pop2',
+            'Pop3', 'Pop4', 'Pop5', 'Pop6', 'Pop7', 'Pop8', 'Pop9', 'Pop10',
+            'Pop11', 'Pop12', 'Pop13', 'Pop14', 'Pop15', 'Pop16', 'Pop17', 'Pop18',
+            'Pop19', 'Pop20', 'Pop21', 'Pop22', 'Pop23', 'Pop24', 'Pop25', 'Pop26',
+            'Lb']
+    pop = pop[cols]
+    pop.columns = pop.columns.map(pop_concept_mapping)
+    pop_ = pop.copy()
+    pop_div_t = pop_div.set_index('All ages').T.reset_index(drop=True)
+
+    pop_ = pop_.rename(columns={'population_age1_year': 'population_age_1_year'})
+    pop_ = pop_.set_index(['country', 'year', 'sex'])
+    return pop_
